438231ee3ce7dfc942027d509d676a6460fda188
Maxime Huran <m.huran@monsieurbiz.com>
2023-06-30 10:25:54 +0200

    Update tests folder

--- tests/Application/config/bootstrap.php
+++ tests/Application/config/bootstrap.php
@@ -1,19 +1,8 @@
 <?php
 
-/*
- * This file is part of Les-Tilleuls.coop's Click 'N' Collect project.
- *
- * (c) Les-Tilleuls.coop <contact@les-tilleuls.coop>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
-declare(strict_types=1);
-
 use Symfony\Component\Dotenv\Dotenv;
 
-require dirname(__DIR__).'../../../vendor/autoload.php';
+require dirname(__DIR__).'/vendor/autoload.php';
 
 // Load cached env vars if the .env.local.php file exists
 // Run "composer dump-env prod" to create it (requires symfony/flex >=1.2)
@@ -22,6 +11,10 @@ if (is_array($env = @include dirname(__DIR__).'/.env.local.php')) {
     $_ENV += $env;
 } elseif (!class_exists(Dotenv::class)) {
     throw new RuntimeException('Please run "composer require symfony/dotenv" to load the ".env" files configuring the application.');
+} elseif (method_exists(Dotenv::class, 'bootEnv')) {
+    (new Dotenv())->bootEnv(dirname(__DIR__) . '/.env');
+
+    return;
 } else {
     // load all the .env files
     (new Dotenv(true))->loadEnv(dirname(__DIR__).'/.env');
@@ -29,4 +22,4 @@ if (is_array($env = @include dirname(__DIR__).'/.env.local.php')) {
 
 $_SERVER['APP_ENV'] = $_ENV['APP_ENV'] = ($_SERVER['APP_ENV'] ?? $_ENV['APP_ENV'] ?? null) ?: 'dev';
 $_SERVER['APP_DEBUG'] = $_SERVER['APP_DEBUG'] ?? $_ENV['APP_DEBUG'] ?? 'prod' !== $_SERVER['APP_ENV'];
-$_SERVER['APP_DEBUG'] = $_ENV['APP_DEBUG'] = (int) $_SERVER['APP_DEBUG'] || filter_var($_SERVER['APP_DEBUG'], \FILTER_VALIDATE_BOOLEAN) ? '1' : '0';
+$_SERVER['APP_DEBUG'] = $_ENV['APP_DEBUG'] = (int) $_SERVER['APP_DEBUG'] || filter_var($_SERVER['APP_DEBUG'], FILTER_VALIDATE_BOOLEAN) ? '1' : '0';
--- tests/Application/public/index.php
+++ tests/Application/public/index.php
@@ -1,38 +1,9 @@
 <?php
 
-/*
- * This file is part of Les-Tilleuls.coop's Click 'N' Collect project.
- *
- * (c) Les-Tilleuls.coop <contact@les-tilleuls.coop>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
+use App\Kernel;
 
-declare(strict_types=1);
+require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
-use Symfony\Component\Debug\Debug;
-use Symfony\Component\HttpFoundation\Request;
-use Tests\CoopTilleuls\SyliusClickNCollectPlugin\Application\Kernel;
-
-require dirname(__DIR__).'/config/bootstrap.php';
-
-if ($_SERVER['APP_DEBUG']) {
-    umask(0000);
-
-    Debug::enable();
-}
-
-if ($trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? $_ENV['TRUSTED_PROXIES'] ?? false) {
-    Request::setTrustedProxies(explode(',', $trustedProxies), Request::HEADER_X_FORWARDED_ALL ^ Request::HEADER_X_FORWARDED_HOST);
-}
-
-if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? $_ENV['TRUSTED_HOSTS'] ?? false) {
-    Request::setTrustedHosts([$trustedHosts]);
-}
-
-$kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
-$request = Request::createFromGlobals();
-$response = $kernel->handle($request);
-$response->send();
-$kernel->terminate($request, $response);
+return function (array $context) {
+    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+};
